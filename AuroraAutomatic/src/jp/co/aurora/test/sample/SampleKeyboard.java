fd12cd2df8b7438145c7a3918a2905acd4f400a3
gaohangdy <gaohangdy@gmail.com>
2013-03-27 03:00:08 -0300

    modify keyboard

--- AuroraAutomatic/src/jp/co/aurora/test/sample/SampleKeyboard.java
+++ AuroraAutomatic/src/jp/co/aurora/test/sample/SampleKeyboard.java
@@ -2,64 +2,37 @@ package jp.co.aurora.test.sample;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
+import jp.co.amway.aurora.test.util.AuroraTestCase;
+
 import org.junit.*;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+/** Passed in firefox, chrome */
+public class SampleKeyboard extends AuroraTestCase {
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
 
-public class SampleKeyboard {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://www.google.com.hk/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testSampleKeyboard() throws Exception {
-    driver.get(baseUrl + "/");
-    driver.findElement(By.id("lst-ib")).clear();
-    driver.findElement(By.id("lst-ib")).sendKeys("thinkpad");
-    driver.findElement(By.id("lst-ib")).sendKeys(Keys.RETURN);
-    // ERROR: Caught exception [ERROR: Unsupported command [keyPress | lst-ib | 13]]
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alert.getText();
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	@Test
+	public void testSampleKeyboard() throws Exception {
+		baseUrl = "http://www.google.com.hk/";
+		driver.get(baseUrl + "/");
+		driver.findElement(By.id("lst-ib")).clear();
+		driver.findElement(By.id("lst-ib")).sendKeys("thinkpad");
+		driver.findElement(By.id("lst-ib")).sendKeys(Keys.TAB);
+		driver.findElement(By.id("lst-ib")).submit();
+		// ERROR: Caught exception [ERROR: Unsupported command [keyPress |
+		// lst-ib | 13]]
+	}
 }
